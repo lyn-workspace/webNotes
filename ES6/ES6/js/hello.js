3f3c066e742baf78622bd7caa17a14753c1ee134
luyanan <luyanan0718@163.com>
2020-08-03 16:33:11 +0800

    es6

---
+++ ES6/ES6/js/hello.js
@@ -0,0 +1,14 @@
+// export const util = {
+// 	sum(a, b) {
+// 		return a + b;
+// 	}   
+// }
+
+export default {
+	sum(a, b) {
+		return a + b;
+	}
+}
+
+// export {util}
+// export 不仅可以到处对象,一切js变量都可以导出, 比如基本类型变量, 函数、数组、对象
---
+++ ES6/ES6/js/user.js
@@ -0,0 +1,14 @@
+var name = "张三"
+var age = 12
+
+function add(a, b) {
+
+	return a + b;
+}
+
+
+export {
+	name,
+	age,
+	add
+}
