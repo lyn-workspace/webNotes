c80b24e0df94b61acc2a3a35ffc1845a7337c213
luyanan <luyanan0718@163.com>
2020-08-07 22:21:21 +0800

    batch comit

---
+++ uviewui/pages/news/newsInfo.vue
@@ -0,0 +1,37 @@
+<template>
+	<!-- <view> 收到的参数为：{{paramsStr.age}}</view> -->
+	<web-view :webview-styles="webviewStyles" :src="id"></web-view>
+</template>
+
+
+<script>
+	export default {
+		data() {
+			return {
+				id: "",
+				params: {}
+
+			}
+		},
+		onLoad(option) {
+			this.params = option;
+			this.id  = this.params.age;
+		},
+		computed: {
+			paramsStr() {
+				return this.params;
+			}
+		},
+
+		methods: {
+
+
+		}
+	};
+</script>
+
+
+
+<style lang="scss" scoped>
+
+</style>
